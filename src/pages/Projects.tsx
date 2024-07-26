da343da9cfe556088e2bdd6460cccec9df305293
InterMaus1154 <amarco1154@gmail.com>
2024-07-26 10:32:16 +0100

    project changes

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,7 +3,7 @@ import PageTemplate from "./PageTemplate";
 import "../styles/component_styles/Projects.css";
 import ProjectBox from "../components/ProjectBox";
 
-interface IProject{
+interface IProject {
     projectTitle: string;
     projectExcerpt: string;
     projectImage: any;
@@ -11,47 +11,61 @@ interface IProject{
     projectGithubLink: string;
 }
 
-const PROJECTS : IProject[] = [
+const PROJECTS: IProject[] = [
+    {
+        projectTitle: "Smile Canine Boarding - Frontend",
+        projectExcerpt: "Dog hotel booking website (fictional). Made with React and TypeScript.",
+        projectImage: "project_images/icon.svg",
+        projectLink: "https://smile-canine-boarding.onrender.com",
+        projectGithubLink: "https://github.com/InterMaus1154/scb_fe"
+    },
+    {
+        projectTitle: "Smile Canine Boarding - Backend (REST Api)",
+        projectExcerpt: "Made with Laravel and MySQL.",
+        projectImage: "project_images/icon.svg",
+        projectLink: "https://smile-canine-boarding.onrender.com",
+        projectGithubLink: "https://github.com/InterMaus1154/scb_be"
+    },
     {
         projectTitle: "DineEase",
         projectExcerpt: "Design implementation of a WorldSkills competition practise task. Made with React and TypeScript",
-        projectImage: "https://i.ibb.co/pxww2Rj/DineEase.png",
+        projectImage: "project_images/DineEase.png",
         projectLink: "https://dineease-9bnx.onrender.com/",
         projectGithubLink: "https://github.com/InterMaus1154/dineease"
     },
     {
         projectTitle: "Multiplayer Snake Game",
         projectExcerpt: "(Local) Multiplayer snake game made in React and TypeScript. To be controlled with WASD and arrow keys",
-        projectImage: "https://i.ibb.co/k2DPXLZ/snake.png",
+        projectImage: "project_images/snake.png",
         projectLink: "https://react-snake-multiplayer.onrender.com/",
         projectGithubLink: "https://github.com/InterMaus1154/react-snake-multiplayer/"
     },
     {
         projectTitle: "Verina To-do App",
         projectExcerpt: "To-do web application, with React on the frontend, Express on the backend, all connected via websockets.",
-        projectImage: "https://i.ibb.co/sFmspFs/todo-logo.png",
+        projectImage: "project_images/todo.png",
         projectLink: "https://verinatodoappbeta.onrender.com/",
         projectGithubLink: "https://github.com/InterMaus1154/react-ts-todo"
     },
     {
         projectTitle: "HotBeans Web Development",
         projectExcerpt: "HotBeansWeb is a fictional company, made for school assignment in React. Designed and implemented by me.",
-        projectImage: "https://i.ibb.co/crxT0HT/hotbeans.png",
+        projectImage: "project_images/hotbeans.png",
         projectLink: "https://hotbeans.onrender.com/",
         projectGithubLink: "https://github.com/InterMaus1154/hotbeans"
     },
-    {
-        projectTitle: "KimCorp Power",
-        projectExcerpt: "Design implementation of a WorldSkills competition practise task. Made with HTML, CSS and minimal JavaScript",
-        projectImage: "https://i.ibb.co/n7R7c2R/kimcorp-logo.png",
-        projectLink: "https://kimcorp-power.onrender.com/",
-        projectGithubLink: "https://github.com/InterMaus1154/kimcorp-worldskills"
-    },
+    // {
+    //     projectTitle: "KimCorp Power",
+    //     projectExcerpt: "Design implementation of a WorldSkills competition practise task. Made with HTML, CSS and minimal JavaScript",
+    //     projectImage: "https://i.ibb.co/n7R7c2R/kimcorp-logo.png",
+    //     projectLink: "https://kimcorp-power.onrender.com/",
+    //     projectGithubLink: "https://github.com/InterMaus1154/kimcorp-worldskills"
+    // },
 ];
 
 const Projects: FC = () => {
 
-    const projects = useMemo(()=> PROJECTS, [PROJECTS]);
+    const projects = useMemo(() => PROJECTS, [PROJECTS]);
 
     return (
         <PageTemplate className={"Projects--section"}>
